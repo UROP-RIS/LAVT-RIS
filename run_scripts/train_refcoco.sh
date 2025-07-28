3ba3906adbe829943551fdb593635796c22164aa
fallengold-dxcpu3 <zguobd@connect.ust.hk>
2025-07-28 15:48:21 +0800

    add git ignore and train script

---
+++ run_scripts/train_refcoco.sh
@@ -0,0 +1,18 @@
+mkdir ./models/refcoco
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --master_port=12345 \
+    train.py \
+    --model lavt \
+    --dataset refcoco \
+    --model_id refcoco \
+    --batch-size 8 \
+    --lr 0.00005 \
+    --wd 1e-2 \
+    --swin_type base \
+    --pretrained_swin_weights ./pretrained_weights/swin_base_patch4_window12_384_22k.pth \
+    --epochs 40 \
+    --img_size 480 \
+    2>&1 | tee ./models/refcoco/output
